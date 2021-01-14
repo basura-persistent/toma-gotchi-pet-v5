ddc33651cf31e45a74e29802d294d64b5afc3a3f
Hector <romero.hector00@icloud.com>
2021-01-14 12:24:28 -0800

    Initial Commit

---
+++ tomagotchi-pet-v6/Views/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  tomagotchi-pet-v6
+//
+//  Created by Rohit Basu on 1/14/21.
+//hector
+// johny boi
+// rohit boi
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tomagotchi-pet-v6/tomagotchi_pet_v6App.swift
@@ -0,0 +1,17 @@
+//
+//  tomagotchi_pet_v6App.swift
+//  tomagotchi-pet-v6
+//
+//  Created by Rohit Basu on 1/14/21.
+//
+
+import SwiftUI
+
+@main
+struct tomagotchi_pet_v6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
